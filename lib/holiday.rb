79684f62f37e1a1bcf4cbc5e0709a621614e5097
Sara A <calisaraalhindi@gmail.com>
2020-08-09 01:58:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -22,7 +22,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  holiday_hash.each do |holiday, decor|
+  holiday_hash[:winter].each do |holiday, decor|
     decor << supply
   end
 end
