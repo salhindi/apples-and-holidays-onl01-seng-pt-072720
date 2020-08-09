9da28b6e5e01840c427938af2088d836e93fe495
Sara A <calisaraalhindi@gmail.com>
2020-08-09 01:57:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -23,7 +23,7 @@ end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash.each do |holiday, decor|
-    decorations << supply
+    decor << supply
   end
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
